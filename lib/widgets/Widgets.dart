abed627f30afff14e58ea78dab75e026b6530a77
ludde <ludde.lindahl@gmail.com>
2021-10-22 01:04:59 +0200

    Added textCenter method

--- lib/widgets/Widgets.dart
+++ lib/widgets/Widgets.dart
@@ -3,9 +3,18 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 // ignore: non_constant_identifier_names
-Widget TextCaption(dynamic context, String text, TextStyle? style) {
+Widget TextCaptionLeft(dynamic context, String text, TextStyle? style) {
   return Align(
     alignment: Alignment.centerLeft,
+    child: Text(
+      text,
+      style: style,
+    ),
+  );
+}
+
+Widget TextCaptionCenter(dynamic context, String text, TextStyle? style) {
+  return Center(
     child: Text(
       text,
       style: style,
@@ -21,9 +30,6 @@ Widget TextInput(dynamic context, String hintText) {
       contentPadding: EdgeInsets.all(5.0),
       isDense: true,
       enabledBorder: UnderlineInputBorder(
-        borderSide: BorderSide(color: Colors.white),
-      ),
-      focusedBorder: UnderlineInputBorder(
         borderSide: BorderSide(color: Colors.white),
       ),
     ),
