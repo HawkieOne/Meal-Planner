75b74f3794a63c86d7a81f5e8a4c4b9ffeec78fd
ludde <ludde.lindahl@gmail.com>
2021-10-22 01:04:20 +0200

    Refactored parts of code

--- lib/widgets/MealInfo.dart
+++ lib/widgets/MealInfo.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:intl/intl.dart';
+import 'package:meal_planner/widgets/Widgets.dart';
 
 import 'package:http/http.dart' as http;
 import 'package:html/parser.dart' as parser;
@@ -10,9 +10,8 @@ import 'package:html/dom.dart' as dom;
 
 class MealInfo extends StatefulWidget {
   final String title;
-  final IconData icon;
 
-  MealInfo({Key? key, this.title: "Undefined", this.icon: Icons.help_outline}) : super(key: key);
+  MealInfo({Key? key, this.title: "Undefined"}) : super(key: key);
 
   @override
   _MealInfoState createState() => _MealInfoState();
@@ -46,7 +45,7 @@ class _MealInfoState extends State<MealInfo> {
   Widget build(BuildContext context) {
     final Map arguments = ModalRoute.of(context)!.settings.arguments as Map;
     String title = arguments["title"];
-    IconData icon = arguments["icon"];
+    IconData icon = Icons.help_outline;
 
     void addIngredientWidget(String ingr, String qty) {
       _ingredients.add(ingredientField(ingr, qty));
@@ -62,7 +61,7 @@ class _MealInfoState extends State<MealInfo> {
         backgroundColor: Theme.of(context).primaryColor,
 
       ),
-      body: SingleChildScrollView(
+      body: Container(
         padding: const EdgeInsets.all(20),
         child: hasUrl == true ? FutureBuilder(
           future: http.get(Uri.parse(url)),
@@ -74,7 +73,7 @@ class _MealInfoState extends State<MealInfo> {
               var ingrList = document.getElementById('ingredients')?.getElementsByClassName('ingredients-list-group')[0].
               getElementsByClassName('ingredients-list-group__card');
 
-              for (var i = 0; i < ingrList!.length - 1; i++) {
+              for (var i = 0; i < ingrList!.length ; i++) {
                 var qty = ingrList[i].getElementsByTagName('span')[0].innerHtml.replaceAll(RegExp('\\s'), "").replaceAll(RegExp("(?<=\\d)(?=\\D)")," ").replaceAll(RegExp("(?<=\\D)(?=\\d)")," ");
                 var ingredient = toBeginningOfSentenceCase(ingrList[i].getElementsByTagName('span')[1].innerHtml.replaceAll(RegExp(r'\n'), "").replaceAll(RegExp(r'  '), ''));
                 qtis.add(qty);
@@ -84,131 +83,99 @@ class _MealInfoState extends State<MealInfo> {
               print(qtis);
               print(ingredients);
 
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+              return SingleChildScrollView(
+                child:  Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
                       children: [
-                        Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text("Meal",
-                            style: Theme.of(context).textTheme.headline3,
-                          ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            TextCaptionLeft(context, "Meal", Theme.of(context).textTheme.headline3),
+                            Padding(padding: EdgeInsets.all(5)),
+                            Container(
+                              width: MediaQuery.of(context).size.width / 2,
+                              child: TextInput(context, "Enter meal here"),
+                            ),
+                          ],
                         ),
-                      Padding(padding: EdgeInsets.all(5)),
-                      Container(
-                        width: MediaQuery.of(context).size.width / 2,
-                        child: TextField(
-                          decoration: InputDecoration(
-                            hintText: "Enter meal here",
-                            contentPadding: EdgeInsets.all(5.0),
-                            isDense: true,
-                            enabledBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white),
-                            ),
-                            focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white),
-                            ),
-                          )
+                        Container(
+                            width: 100,
+                            height: 100,
+                            child: Image.network(img.attributes['src'].toString(),
+                                fit: BoxFit.cover
+                            )
+                        ),
+                      ],
+                    ),
+                    Padding(padding: const EdgeInsets.all(10)),
+                    Container (
+                      width: MediaQuery.of(context).size.width * 0.85,
+                      child: Row(
+                        children: [
+                          Expanded(flex: 2,child: TextCaptionLeft(context, "Amount", DefaultTextStyle.of(context).style)),
+                          Expanded(flex: 8,child: TextCaptionCenter(context, "Ingredients", DefaultTextStyle.of(context).style)
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.9,
+                      child: ListView.builder(
+                        itemCount: _ingredients.length,
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        padding: const EdgeInsets.all(8),
+                        itemBuilder: (BuildContext context, int index){
+                          return _ingredients[index];
+                        },
+                      ),
+                    ),
+                    Padding(padding: const EdgeInsets.all(10)),
+                    TextCaptionLeft(context, "Notes", DefaultTextStyle.of(context).style),
+                    Padding(padding: const EdgeInsets.all(5)),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.9,
+                      child: TextField(
+                        maxLines: 5,
+                        decoration: InputDecoration.collapsed(
+                          filled: true,
+                          fillColor: Colors.grey.shade800,
+                          hintText: "Enter your notes here",
                         ),
                       ),
-                    ],
-                  ),
-                Container(
-                  width: 100,
-                  height: 100,
-                  child: Image.network(img.attributes['src'].toString(),
-                  fit: BoxFit.cover
-                  )
-                ),
-              ],
-            ),
-            Padding(padding: const EdgeInsets.all(10)),
-            Container (
-              width: MediaQuery.of(context).size.width * 0.85,
-              child: Row(
-                children: [
-                  Expanded(flex: 2,child: Align(alignment: Alignment.centerLeft,child: Text("Amount"))),
-                  Expanded(flex: 8,child: Align(
-                    alignment: Alignment.center,
-                    child: Text("Ingredients"))
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width * 0.9,
-              // height: MediaQuery.of(context).size.height * 0.5,
-              child: ListView.builder(
-                itemCount: _ingredients.length,
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                padding: const EdgeInsets.all(8),
-                itemBuilder: (BuildContext context, int index){
-                  return _ingredients[index];
-                },
-              ),
-            ),
-            // ElevatedButton(
-            //   style: ElevatedButton.styleFrom(
-            //     // shape: BeveledRectangleBorder(),
-            //     fixedSize: Size.fromWidth(MediaQuery.of(context).size.width / 3)
-            //   ),
-            //   child: const Icon(Icons.add),
-            //   onPressed: () { addIngredientWidget(); },
-            // ),
-            Padding(padding: const EdgeInsets.all(10)),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Text("Notes"),
-            ),
-            Padding(padding: const EdgeInsets.all(5)),
-            Container(
-              width: MediaQuery.of(context).size.width * 0.9,
-              child: TextField(
-                maxLines: 5,
-                  decoration: InputDecoration.collapsed(
-                  filled: true,
-                  fillColor: Colors.grey.shade800,
-                  hintText: "Enter your notes here",
-                ),
-              ),
-            ),
-            Padding(padding: const EdgeInsets.all(5)),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Colors.green,
-                fixedSize: Size.fromWidth(MediaQuery.of(context).size.width / 2),
-              ),
-              child: const Text("Save"),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-            ],
-            );
-
-          }
-            else {
-              return Container(
-                width: 50,
-                height: 50,
-                child: Center(
-                  child: CircularProgressIndicator(
-                    color: Colors.blue,
-                  )
+                    ),
+                    Padding(padding: const EdgeInsets.all(5)),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.green,
+                        fixedSize: Size.fromWidth(MediaQuery.of(context).size.width / 2),
+                      ),
+                      child: const Text("Save"),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ],
                 ),
               );
-           }
+            }
+            else {
+              return Center(
+                child: CircularProgressIndicator(
+                  color: Colors.blue,
+                )
+              );
+            }
           },
-        ) : Column(
+        ) : Center(
+            child: Column(
               children: [
-                Text("Enter meal"),
+                TextCaptionCenter(context, "Enter meal", Theme.of(context).textTheme.headline3),
                 TextFormField(controller: _controller),
+                Padding(padding: EdgeInsets.all(5)),
                 ElevatedButton(
                   onPressed: () {
                     setState(() {
@@ -216,9 +183,10 @@ class _MealInfoState extends State<MealInfo> {
                       hasUrl = true;
                     });
                   },
-                child: Text("Submit"),
+                  child: Text("Submit"),
                 ),
-            ],
+              ],
+            ),
           ),
       ),
     );
